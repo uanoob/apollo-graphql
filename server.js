d728561bef38387e09daef1598b733f41fbe9d28
uanoob <usnet@ukr.net>
2018-12-19 11:36:06 +0200

    Connect mongoose to database

--- server.js
+++ server.js
@@ -1,8 +1,25 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const { PORT, MONGO_URI } = require('./config');
+const Recipe = require('./models/Recipe');
+const User = require('./models/User');
+
+mongoose
+  .connect(
+    MONGO_URI,
+    {
+      useNewUrlParser: true,
+      useCreateIndex: true
+    }
+  )
+  .then(() => {
+    console.log('DB connected');
+  })
+  .catch(error => {
+    console.log(error);
+  });
 
 const app = express();
-
-const PORT = process.env.PORT || 8000;
 
 app.listen(PORT, () => {
   console.log(`Server listening on PORT ${PORT}`);
