a08b3d07cb04980ced332ecad5d4a644d5c802b9
uanoob <usnet@ukr.net>
2018-12-20 12:34:17 +0200

    Create ApolloServer & connect to app & Create objectIdToString function

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 module.exports = {
   PORT: process.env.PORT || 8000,
-  MONGO_URI: process.env.MONGO_URI || 'mongodb://localhost/apollo-graphql'
+  MONGO_URI: process.env.MONGO_URI || 'mongodb://localhost/apollo-graphql',
 };
--- server.js
+++ server.js
@@ -1,25 +1,48 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const { ApolloServer } = require('apollo-server-express');
+const { typeDefs } = require('./schema');
+const { resolvers } = require('./resolvers');
 const { PORT, MONGO_URI } = require('./config');
 const Recipe = require('./models/Recipe');
 const User = require('./models/User');
+
+const { ObjectId } = mongoose.Types;
+function objectIdToString() {
+  return this.toString();
+}
+ObjectId.prototype.valueOf = objectIdToString;
 
 mongoose
   .connect(
     MONGO_URI,
     {
       useNewUrlParser: true,
-      useCreateIndex: true
-    }
+      useCreateIndex: true,
+    },
   )
   .then(() => {
     console.log('DB connected');
   })
-  .catch(error => {
+  .catch((error) => {
     console.log(error);
   });
 
 const app = express();
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: {
+    Recipe,
+    User,
+  },
+  playground: {
+    endpoint: '/graphql',
+  },
+});
+
+server.applyMiddleware({ app });
 
 app.listen(PORT, () => {
   console.log(`Server listening on PORT ${PORT}`);
