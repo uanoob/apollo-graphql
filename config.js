7590a2849302471a3434c3a37a2073ebf5fc92df
uanoob <usnet@ukr.net>
2018-12-21 17:24:14 +0200

    Server: Add CORS_ORIGIN to config

--- config.js
+++ config.js
@@ -2,4 +2,5 @@ module.exports = {
   PORT: process.env.PORT || 8000,
   MONGO_URI: process.env.MONGO_URI || 'mongodb://localhost/apollo-graphql',
   JWT_KEY: process.env.JWT_KEY || 'dwAWedGFd32GJKG23Dfgt568edFDsrswOl876gGtTer',
+  CORS_ORIGIN: process.env.CORS_ORIGIN || 'http://localhost:3000',
 };
