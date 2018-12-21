f5efb8aa3c56c0678f920b2f617bf2210929b824
uanoob <usnet@ukr.net>
2018-12-21 12:54:55 +0200

    Fix -> Change function to arrow func -> bcrypt.hash

--- models/User.js
+++ models/User.js
@@ -27,13 +27,13 @@ const UserSchema = new Schema(
 );
 
 /* eslint-disable */
-UserSchema.pre('save', function(next) {
+UserSchema.pre('save', function (next) {
   if (!this.isModified('password')) {
     return next();
   }
   bcrypt.genSalt(10, (err, salt) => {
     if (err) return next(err);
-    bcrypt.hash(this.password, salt, function(err, hash) {
+    bcrypt.hash(this.password, salt, (err, hash) => {
       if (err) return next(err);
       this.password = hash;
       next();
