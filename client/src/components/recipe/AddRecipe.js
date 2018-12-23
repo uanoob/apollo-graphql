c94f31eb9081c59fb0b6ce441763170e1662c78c
uanoob <usnet@ukr.net>
2018-12-23 13:03:41 +0200

    Client: Create implementations -> AddRecipe & manual updateCache

--- client/src/components/recipe/AddRecipe.js
+++ client/src/components/recipe/AddRecipe.js
@@ -1,5 +1,148 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { withRouter } from 'react-router-dom';
+import { PropTypes } from 'prop-types';
+import { Mutation } from 'react-apollo';
+import { ADD_RECIPE, GET_ALL_RECIPES } from '../../queries';
+import Error from '../Error';
 
-const AddRecipe = () => <div>AddRecipe</div>;
+const initialState = {
+  name: '',
+  category: 'Breakfast',
+  description: '',
+  instructions: '',
+  username: '',
+};
 
-export default AddRecipe;
+class AddRecipe extends Component {
+  state = {
+    ...initialState,
+  };
+
+  componentDidMount() {
+    const { session } = this.props;
+    if (session.getCurrentUser) {
+      const { username } = session.getCurrentUser;
+      this.setState({
+        username,
+      });
+    }
+  }
+
+  handleChange = (e) => {
+    const { name, value } = e.target;
+    this.setState({
+      [name]: value,
+    });
+  };
+
+  clearState = () => {
+    this.setState({ ...initialState });
+  };
+
+  handleSubmit = (e, addRecipe) => {
+    e.preventDefault();
+    const { history } = this.props;
+    addRecipe().then(() => {
+      this.clearState();
+      history.push('/');
+    });
+  };
+
+  updateCache = (cache, { data: { addRecipe } }) => {
+    const { getAllRecipes } = cache.readQuery({ query: GET_ALL_RECIPES });
+    cache.writeQuery({
+      query: GET_ALL_RECIPES,
+      data: {
+        getAllRecipes: [addRecipe, ...getAllRecipes],
+      },
+    });
+  };
+
+  validateForm = () => {
+    const { name, description, instructions } = this.state;
+    const isInvalid = !name || !description || !instructions;
+    return isInvalid;
+  };
+
+  render() {
+    const {
+      name, category, description, instructions, username,
+    } = this.state;
+    return (
+      <div className="App">
+        <Mutation
+          mutation={ADD_RECIPE}
+          variables={{
+            name,
+            description,
+            category,
+            instructions,
+            username,
+          }}
+          update={this.updateCache}
+        >
+          {(addRecipe, { loading, error }) => (
+            <form className="form" onSubmit={e => this.handleSubmit(e, addRecipe)}>
+              <input
+                type="text"
+                name="name"
+                value={name}
+                placeholder="Recipe name"
+                onChange={this.handleChange}
+              />
+              <select name="category" value={category} onChange={this.handleChange}>
+                <option value="Breakfast">Breakfast</option>
+                <option value="Lunch">Lunch</option>
+                <option value="Dinner">Dinner</option>
+                <option value="Snack">Snack</option>
+              </select>
+              <input
+                type="text"
+                name="description"
+                value={description}
+                placeholder="Add description"
+                onChange={this.handleChange}
+              />
+              <textarea
+                type="text"
+                name="instructions"
+                value={instructions}
+                placeholder="Add instructions"
+                onChange={this.handleChange}
+              />
+              <button
+                type="submit"
+                // className="button-primary"
+                disabled={loading || this.validateForm()}
+              >
+                Submit
+              </button>
+              {error && <Error error={error} />}
+            </form>
+          )}
+        </Mutation>
+      </div>
+    );
+  }
+}
+
+AddRecipe.defaultProps = {
+  session: PropTypes.shape({
+    getCurrentUser: null,
+  }),
+};
+
+AddRecipe.propTypes = {
+  session: PropTypes.shape({
+    getCurrentUser: PropTypes.shape({
+      created_at: PropTypes.string.isRequired,
+      email: PropTypes.string.isRequired,
+      username: PropTypes.string.isRequired,
+    }),
+  }),
+  history: PropTypes.shape({
+    push: PropTypes.func.isRequired,
+  }).isRequired,
+};
+
+export default withRouter(AddRecipe);
--- client/src/index.js
+++ client/src/index.js
@@ -14,6 +14,7 @@ import Search from './components/recipe/Search';
 import Login from './components/auth/Login';
 import Signup from './components/auth/Signup';
 import AddRecipe from './components/recipe/AddRecipe';
+import RecipePage from './components/recipe/RecipePage';
 import Profile from './components/profile/Profile';
 import withSession from './components/withSession';
 
@@ -49,7 +50,8 @@ const Root = ({ refetch, session }) => (
         <Route path="/search" component={Search} />
         <Route path="/login" render={() => <Login refetch={refetch} />} />
         <Route path="/signup" render={() => <Signup refetch={refetch} />} />
-        <Route path="/recipe/add" component={AddRecipe} />
+        <Route path="/recipe/add" render={() => <AddRecipe session={session} />} />
+        <Route path="/recipes/:_id" component={RecipePage} />
         <Route path="/profile" component={Profile} />
         <Redirect to="/" />
       </Switch>
